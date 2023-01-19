1f414875d7880f61a97a2f88ce4135769329b0bf
Franck TCHIENGUEN <98441969+FranckTCHIENGUEN@users.noreply.github.com>
2023-01-19 14:38:05 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'GestionCommerciale-PWA',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
